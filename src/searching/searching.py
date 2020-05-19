d4b0613525759b8cb05f8be23dec6bfc80f16f52
eyvonne <eyvonne@users.noreply.github.com>
2020-05-18 17:21:03 -0700

    searching added

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,22 @@
 def linear_search(arr, target):
-    # Your code here
-
+    for i, j in enumerate(arr):
+        if j == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr)-1
 
-    # Your code here
-
-
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        elif arr[mid] > target:
+            high = mid - 1
     return -1  # not found
